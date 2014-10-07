1166aa9046336cd4f97ee3e966e57dd0c4b12c96
Roy McFarland <roy.mcfarland@gmail.com>
2014-10-07 13:21:52 -0600

    added skeleton structuring for static research portal

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 $(document).on('ready', function() {
 	
-	// Inserts username / password into login box
+	// Inserts static username & password @signIn
 	$(".username").val("roy_mcfarland")
 	$(".password").val("************")
-  
+
+
+
+	// Takes user to research portal  
 	$(document).on('click', '#myLogin', function(e){
 		e.preventDefault();
 		window.location.replace('file:///Users/student/projects/week5/midterm/research-portal.html');
