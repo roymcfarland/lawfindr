5feaa77b8cf02a465e96f4036b3351723f8191fe
Roy McFarland <roy.mcfarland@gmail.com>
2014-10-09 16:09:14 -0600

    UX/UI improvements (generally): added levity button, cropped images & used jQuery slideDown() to reveal case results

--- main.js
+++ main.js
@@ -4,56 +4,131 @@
 
 var elementsOfMurder = '<div class="row rowBreak">' +
 	'<div class="col-xs-2 elements"><span class="label label-success">Elements:</span></div>' +
-	'<div class="col-xs-2" id="action"><button type="button" class="btn btn-default">Action</button></div>' +
-	'<div class="col-xs-2" id="intent"><button type="button" class="btn btn-default">Intent</button></div>' +
-	'<div class="col-xs-2" id="causation"><button type="button" class="btn btn-default">Causation</button></div>' +
-	'<div class="col-xs-2"><button type="button" class="btn btn-default popover-btn" data-container="body" data-toggle="popover" data-placement="top" data-content="The victim must be an actual living person. In certain states, this includes a fetus.">Victim*</button></div>' +
-	'<div class="col-xs-1"><button type="button" class="btn btn-default popover-btn" data-container="body" data-toggle="popover" data-placement="top" data-content="In most jurisdictions, the victim must die within one year of the defendant\'s actions.">Timing*</button></div>' +
+	'<div class="col-xs-offset-1 col-xs-2" id="action"><button type="button" class="btn btn-default">Action</button></div>' +
+	'<div class="col-xs-offset-1 col-xs-2" id="intent"><button type="button" class="btn btn-default">Intent</button></div>' +
+	'<div class="col-xs-offset-1 col-xs-2" id="causation"><button type="button" class="btn btn-default">Causation</button></div>' +
 	'</div>';
 
 var actionResult = '<div class="row rowBreak">' +
-	'<div class="col-xs-offset-2 col-xs-2"><button type="button" class="btn btn-default" id="action1">Voluntary Action</button></div>' +
-	'<div class="col-xs-offset-1 col-xs-2"><button type="button" class="btn btn-default" id="action2">Involuntary Action</button></div>' +
-	'<div class="col-xs-offset-1 col-xs-2"><button type="button" class="btn btn-default" id="action3">Failure to Act</button></div>' +
-	'<div class="col-xs-offset-1 col-xs-1"><span class="glyphicon glyphicon-remove removeButton"></span></div>'
-	'</div>';
-
-var intentResult = '<div class="row rowBreak">' +
-	'<div class="col-xs-offset-2 col-xs-2" id="intent1"><button type="button" class="btn btn-default">Intent to Kill</button></div>' +
-	'<div class="col-xs-2" id="intent2"><button type="button" class="btn btn-default">Intent to Cause Serious Injury</button></div>' +
-	'<div class="col-xs-offset-1 col-xs-2" id="intent3"><button type="button" class="btn btn-default">Depraved Heart</button></div>' +
-	'<div class="col-xs-2" id="intent4"><button class="btn btn-default" data-toggle="modal" data-target=".bs-example-modal-lg">Doctrine of Felony Murder</button></div>' +
-		'<div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">' +
-  		'<div class="modal-dialog modal-lg">' +
-    	'<div class="modal-content">' +
-      		'<h3 class="modal-header">Doctrine of Felony Murder</h3>' +
-      		'<ul><b>Definition:</b> An unintentional killing caused during the commission or attempted commission of a serious or inherently dangerous felony.</ul>' +
-      		'<ul><b>Limitations:</b> Several limitations have been placed on the scope of this rule.</ul>' +
-      		'<ul>(1) <em>Independence:</em> The underlying felony must be independent of the act causing the death. Therefore, where the underlying felony is murder, manslaughter, or aggravated assault, the defendant cannot be guily of felony murder.</ul>' +
-      		'<ul>(2) <em>Inherently Dangerous Felony:</em> The underlying felony must be an inherently dangerous one - such as buglary, arson, robbery, rape, or kidnapping.</ul>' +
-      		'<ul>(3) <em>Foreseeability:</em> The resulting death must be a foreseeable outgrowth of the defendant\'s actions. Most courts have generally been very liberal at applying the foreseeability requirement. Therefore, most deaths are considered foreseeable for purposes of felony murder.</ul>' +
-      		'<ul>(4) <em>Timing:</em> The resulting death must occur during the commission of the felony. If a killing occurs while the defendant is fleeing from the scene of a felony, he may still be guilty of felony murder. However, if he has reached a place of temporary safety, the felony is deemed to have terminated and the defendant can no longer be found guilt of felony murder.</ul>' +
-      		'<ul>(5) <em>Defenses:</em> Any defense to the underlying felony negates the felony murder.</ul>' +
-      		'<ul>(6) <em>Redline Limitation:</em> A co-felon is not guilty of felony murder when the killing constitutes a justifiable or excusable homicide. For example, when the police or the victim shoot one of the co-felons but not when the killing is done by one of the felons.</ul>' +
+  	'<div class="col-xs-offset-2 col-xs-3">' +
+    	'<div class="thumbnail">' +
+      		'<img src="images/action1.jpg">' +
+      	'<div class="caption">' +
+        	'<h3>Voluntary Action</h3>' +
+        	'<p>No crime can be committed by bad thoughts alone. Instead, the defendant\'s act must result from his or her conscious choice. The choice need not be the product of thorough deliberation but may stem from an impulse. For example, a person who suddenly slips on a mountain trail and reaches out to grab the arm of a bystander to avoid falling has acted voluntarily because his mind has quickly grasped the situation and dictated a response.</p>' +
+        	'<ul id="cases-action1">' +
+        		'<li>Case 1:</li>' +
+        	'</ul>' +
+        	'<p><a href="#" class="btn btn-primary" id="action1" role="button">Cases</a></p>' +
+      	'</div>' +
     	'</div>' +
-  		'</div>' +
-		'</div>' +
+  	'</div>' +
+  	 '<div class="col-xs-3">' +
+    	'<div class="thumbnail">' +
+      		'<img src="images/action2.jpg">' +
+      	'<div class="caption">' +
+        	'<h3>Involuntary Action</h3>' +
+        	'<p>Acts over which a person has no physical or mental control are not voluntary. A muscle reflex driven by the autonomic nervous system, such as a knee jerk, is not considered voluntary under the law. Acts committed during seizures, convulsions, hypnosis, or unconscious mental states also lack sufficient volition and judgment needed to impose criminal liability. For the same reasons, acts committed during episodes of sleepwalking are not considered voluntary.</p>' +
+        	'<ul id="cases-action2">' +
+        		'<li>California v. Ebanks: the court rejected defendant\'s argument that he acted involuntarily when murder ex-wife because he was drunk and hypnotized. The judge bluntly remarked that \"[t]he law of the United States does not recognize hypnotism.\"</li>' +
+        	'</ul>' +
+        	'<p><a href="#" class="btn btn-primary" id="action2" role="button">Cases</a></p>' +
+      	'</div>' +
+    	'</div>' +
+  	'</div>' +
+  	'<div class="col-xs-3">' +
+    	'<div class="thumbnail">' +
+      		'<img src="images/action3.jpg">' +
+      	'<div class="caption">' +
+        	'<h3>Failure to Act</h3>' +
+        	'<p>A person who creates a dangerous situation may be under a duty to take reasonable steps to avert that danger. Similarly, there may be a statutory duty to prevent harm when the defendant is acting under a contractual duty or other form of fiduciary responsibility.</p>' +
+        	'<ul id="cases-action3">' +
+        		'<li>R v. Miller: Defendant was guilty of murder for burning down an apartment building. He fell asleep on his mattress while smoking a cigarette. When he woke, he found that the mattress was smouldering but, instead of calling for help, he simply moved into another room, which allowed the fire to spread.</li>' +
+        		'<li>R v. Pittwood: Defendant was convicted of manslaughter after he failed to close the gate on a level crossing as he was contracted to do. This caused a train to collide with a hay cart, and the court ruled that a man \'might\' incur criminal liability from a duty arising out of contract.</li>' +
+        	'</ul>' +
+        	'<button type="button" class="btn btn-primary" id="action3" role="button">Cases</button>' +
+      	'</div>' +
+    	'</div>' +
+  	'</div>' +
 	'<div class="col-xs-1"><span class="glyphicon glyphicon-remove removeButton"></span></div>'
 	'</div>';
 
-var causationResult = '<div class="row rowBreak">' +
-	'<div class="col-xs-offset-2 col-xs-2" id="causation1"><button type="button" class="btn btn-default popover-btn" data-container="body" data-toggle="popover" data-placement="top" data-content="But-For Test: The victim\'s death would not have occurred but for the defendant\'s actions.">Cause-in-Fact*</button></div>' +
-	'<div class="col-xs-2" id="causation2"><button type="button" class="btn btn-default popover-btn" data-container="body" data-toggle="popover" data-placement="top" data-content="When the victim\'s death was a natural and probable consquence of the defendant\'s conduct, the defendant may be guilty of murder even if he did not foresee the exact chain of events that resulted in the victim\'s death.">Proximate Cause*</button></div>' +
-	'<div class="col-xs-2" id="causation3"><button type="button" class="btn btn-default popover-btn" data-container="body" data-toggle="popover" data-placement="top" data-content="When two or more people act simulataneously - yet independently - each may be guilty of murder.">Simultaneous Acts*</button></div>' +
-	'<div class="col-xs-2" id="causation4"><button type="button" class="btn btn-default popover-btn" data-container="body" data-toggle="popover" data-placement="top" data-content="Unusual health conditions do not affect criminal liability.">Pre-Existing Condition*</button></div>' +
-	'<div class="col-xs-offset-1 col-xs-1"><span class="glyphicon glyphicon-remove removeButton"></span></div>'
+var intentResult = '<div class="row rowBreak">' +
+	'<div class="col-xs-offset-2 col-xs-3">' +
+    	'<div class="thumbnail">' +
+      		'<img src="images/intent1.png">' +
+      	'<div class="caption">' +
+        	'<h3>Intent to Kill</h3>' +
+        	'<p>A person who creates a dangerous situation may be under a duty to take reasonable steps to avert that danger. Similarly, there may be a statutory duty to prevent harm when the defendant is acting under a contractual duty or other form of fiduciary responsibility.</p>' +
+        	'<ul id="cases-intent1">' +
+        		'<li></li>' +
+        		'<li></li>' +
+        	'</ul>' +
+        	'<button type="button" class="btn btn-primary" id="intent1" role="button">Cases</button>' +
+      	'</div>' +
+    	'</div>' +
+  	'</div>' +
+  	'<div class="col-xs-offset-2 col-xs-3">' +
+    	'<div class="thumbnail">' +
+      		'<img src="images/intent2.jpg">' +
+      	'<div class="caption">' +
+        	'<h3>Intent to Cause Serious Injury</h3>' +
+        	'<p>If a defendant kills another person unintentionally, he or she can still be guilty of murder if it can be shown that the defendant intended to cause great bodily injury to the victim.</p>' +
+        	'<ul id="cases-intent2">' +
+        		'<li></li>' +
+        		'<li></li>' +
+        	'</ul>' +
+        	'<button type="button" class="btn btn-primary" id="intent2" role="button">Cases</button>' +
+      	'</div>' +
+    	'</div>' +
+  	'</div>' +
+	'<div class="col-xs-offset-1 col-xs-1"><span class="glyphicon glyphicon-remove removeButton"></span></div>' +
 	'</div>';
 
-var cases = '<div class="row rowBreak">' +
-	'<div class="col-xs-offset-2 col-xs-2"><button type="button" class="btn btn-default" id="case1">Jones v. United States <br> 526 US 227 (1999)</button></div>' +
-	'<div class="col-xs-offset-1 col-xs-2"><button type="button" class="btn btn-default" id="case2">United States v. Bailey <br> 444 US 394 (1980)</button></div>' +
-	'<div class="col-xs-offset-1 col-xs-2"><button type="button" class="btn btn-default" id="case3">Holloway v. United States <br> 526 US 1 (1999)</button></div>' +
-	'<div class="col-xs-offset-1 col-xs-1"><span class="glyphicon glyphicon-remove removeButton"></span></div>'
+var causationResult = '<div class="row rowBreak">' +
+	'<div class="col-xs-offset-2 col-xs-3">' +
+    	'<div class="thumbnail">' +
+      		'<img src="images/causation1.jpg">' +
+      	'<div class="caption">' +
+        	'<h3>Cause-in-Fact</h3>' +
+        	'<p>But-For Test: The victim\'s death would not have occurred but for the defendant\'s actions.</p>' +
+        	'<ul id="cases-causation1">' +
+        		'<li></li>' +
+        		'<li></li>' +
+        	'</ul>' +
+        	'<button type="button" class="btn btn-primary" id="causation1" role="button">Cases</button>' +
+      	'</div>' +
+    	'</div>' +
+  	'</div>' +
+	'<div class="col-xs-3">' +
+    	'<div class="thumbnail">' +
+      		'<img src="images/causation2.jpg">' +
+      	'<div class="caption">' +
+        	'<h3>Proximate Cause</h3>' +
+        	'<p>When the victim\'s death was a natural and probable consquence of the defendant\'s conduct, the defendant may be guilty of murder even if he did not foresee the exact chain of events that resulted in the victim\'s death.</p>' +
+        	'<ul id="cases-causation2">' +
+        		'<li></li>' +
+        		'<li></li>' +
+        	'</ul>' +
+        	'<button type="button" class="btn btn-primary" id="causation2" role="button">Cases</button>' +
+      	'</div>' +
+    	'</div>' +
+  	'</div>' +
+	'<div class="col-xs-3">' +
+    	'<div class="thumbnail">' +
+      		'<img src="images/causation3.jpg">' +
+      	'<div class="caption">' +
+        	'<h3>Simultaneous Events</h3>' +
+        	'<p>When two or more people act simulataneously - yet independently - each may be guilty of murder.</p>' +
+        	'<ul id="cases-causation3">' +
+        		'<li></li>' +
+        		'<li></li>' +
+        	'</ul>' +
+        	'<button type="button" class="btn btn-primary" id="causation3" role="button">Cases</button>' +
+      	'</div>' +
+    	'</div>' +
+  	'</div>' +
+	'<div class="col-xs-1"><span class="glyphicon glyphicon-remove removeButton"></span></div>' +
 	'</div>';
 
 
@@ -84,21 +159,6 @@ $(document).on('ready', function() {
 		window.location.replace('file:///Users/student/projects/week5/midterm/index.html')
 	});
 
-	// Takes user @ Research Portal  
-	$(document).on('click', '#case1', function(){
-		window.location.replace('http://scholar.google.com/scholar_case?case=15035002282262379891');
-	});
-
-	// Takes user @ Research Portal  
-	$(document).on('click', '#case2', function(){
-		window.location.replace('http://scholar.google.com/scholar_case?case=1379329779334955251');
-	});
-
-	// Takes user @ Research Portal  
-	$(document).on('click', '#case3', function(){
-		window.location.replace('http://scholar.google.com/scholar_case?case=8614464154510334483');
-	});
-
 
 
 ///////////////////////////////////////////////
@@ -114,15 +174,17 @@ $(document).on('ready', function() {
 		$('[data-toggle=popover]').popover();
 	});
 
+
 /**
  * Appends research results to DOM
  * @return {constituent elements} of murder
  */
-	$(document).on('click', '#searchButton', function(){
+	$(document).on('click', '#search', function(){
 		$('#searchText').val('');
 		// $('#searchInstructions').remove();
 		$('#resultSpace').append(elementsOfMurder);
 	});
+
 
 /**
  * Appends research results to DOM
@@ -132,21 +194,56 @@ $(document).on('ready', function() {
 		$('#resultSpace').append(actionResult);
 	});
 
+	$(document).on('click', '#action1', function(){
+		$('#cases-action1').slideDown('slow');
+	});
+
+	$(document).on('click', '#action2', function(){
+		$('#cases-action2').slideDown('slow');
+	});
+
+	$(document).on('click', '#action3', function(){
+		$('#cases-action3').slideDown('slow');
+	});
+
+
 /**
  * Renders research results to DOM
- * @return {#intent 1-4} that qualify as murderous
+ * @return {#intent 1-2} that qualify as murderous
  */
 	$(document).on('click', '#intent', function(){
     	$('#resultSpace').append(intentResult);
 	});
 
+	$(document).on('click', '#intent1', function(){
+		$('#cases-intent1').slideDown('slow');
+	});
+
+	$(document).on('click', '#intent2', function(){
+		$('#cases-intent2').slideDown('slow');
+	})
+
+
 /**
  * Renders research results to DOM
- * @return {#causation 1-4} that qualify as murderous
+ * @return {#causation 1-3} that qualify as murderous
  */
 	$(document).on('click', '#causation', function(){
 		$('#resultSpace').append(causationResult);
 	});
+
+	$(document).on('click', '#causation1', function(){
+		$('#cases-causation1').slideDown('slow');
+	});
+
+	$(document).on('click', '#causation2', function(){
+		$('#cases-causation2').slideDown('slow');
+	});
+
+	$(document).on('click', '#causation3', function(){
+		$('#cases-causation3').slideDown('slow');
+	});
+
 
 /**
  * Renders research results to DOM
@@ -156,6 +253,7 @@ $(document).on('ready', function() {
     	$('#resultSpace').append(cases);
 	});
 
+
 /**
  * Used to remove a specific row of results from DOM
  * @return {} when button is clicked
@@ -163,5 +261,4 @@ $(document).on('ready', function() {
 	$(document).on('click', '.removeButton', function(){
 		$(this).closest('.row').remove();
 	});
-
 });
